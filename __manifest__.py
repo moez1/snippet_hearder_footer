ae86657db25a893e24085ff87a1491cb5e5c6502
moez <moez.laabidi@gmail.com>
2018-07-25 16:09:52 +0100

    Module snippet header and footer

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'snippet header footer',
+    'version': '1.1',
+    'category': 'account',
+    'sequence': 35,
+    'summary': 'snippet odoo',
+    'author': 'Moez Labidi',
+    'description': """
+entretien odoo
+""",
+    'website': '',
+    'depends': ['website'],
+    'data': [
+        'views/template_snippet_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
